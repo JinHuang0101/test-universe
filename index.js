39c509d81df46de4c352838d3576edd2ee27bc68
JinHuang0101 <jinhuang922@gmail.com>
2023-10-19 15:05:39 -0700

    make it better

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require("express");
 const app = express();
 const { readFile } = require("fs").promises;
 
+/*
 app.get("/", async (request, response) => {
-  response.send(await readFile("./home.html", "utf8"));
+    response.send(await readFile("./home.html", "utf8"));
+});
+*/
+app.get("/", (req, res) => {
+  res.send("The Invisible Universe");
 });
 
 app.listen(process.env.PORT || 3000, () =>
